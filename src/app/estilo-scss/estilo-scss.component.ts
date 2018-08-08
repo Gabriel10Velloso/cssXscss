8776ae76ceb887d94b421abe666371da6dcdcaa0
Gabriel Velloso <gavelloso@gmail.com>
2018-08-08 00:06:47 -0300

    2 Subindo Exemplo:2

--- src/app/estilo-scss/estilo-scss.component.ts
+++ src/app/estilo-scss/estilo-scss.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-estilo-scss',
   templateUrl: './estilo-scss.component.html',
-  styleUrls: ['./estilo-scss.component.scss' , './estilo-scss/media-queries.scss']
+  styleUrls: ['./estilo-scss.component.scss']
 })
 export class EstiloScssComponent implements OnInit {
 
